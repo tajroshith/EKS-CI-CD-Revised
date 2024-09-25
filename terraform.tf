b94dfbaaff1024b5340d63ab1a69c55e76afd572
tajroshith <tajroshith@gmail.com>
2024-09-25 15:16:13 +0530

    Added IaC codes for EKS

--- terraform.tf
+++ terraform.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "eks-terraform-state-s3"
+    bucket         = "wp-eks-terraform-state-s3"
     dynamodb_table = "tfstate-locking"
     region         = "ap-south-1"
 #   profile        = "tfstate-user" Since we are executing from an EC2 Instance we have created a IAM role and assigned the same to the EC2 Instance.
